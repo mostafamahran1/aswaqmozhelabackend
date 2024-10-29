020accd0cfa1091e23160ff34a87d3626fc3a16b
Mostafa Mahran <mahranmostafa900@gmail.com>
2024-10-29 15:26:24 +0300

    Initial commit

--- allproducts/views.py
+++ allproducts/views.py
@@ -11,40 +11,53 @@ from products.models import Product
 from spices.models import SpicesProduct
 from supermarket.models import SupermarketProduct
 from toys.models import ToysProduct
+from accessories.models import AccessoriesProduct
+from gifts.models import GiftsProduct
+from birthday.models import BirthdayProduct
+from socks.models import SocksProduct
+from veils.models import VeilsProduct
 
 def search_all_products(request):
     query = request.GET.get('name', '')
     if not query:
         return JsonResponse({"products": []})
     
-    phones = PhonesProduct.objects.filter(name__icontains=query)
-    clothes = ClothesProduct.objects.filter(name__icontains=query)
-    foods = FoodsProduct.objects.filter(name__icontains=query)
-    fav = FavProduct.objects.filter(name__icontains=query)
-    pharmacy = PharmacyProduct.objects.filter(name__icontains=query)
-    libraryproducts = Product.objects.filter(name__icontains=query)
-    spices = SpicesProduct.objects.filter(name__icontains=query)
-    supermarket = SupermarketProduct.objects.filter(name__icontains=query)
-    toys = ToysProduct.objects.filter(name__icontains=query)
+    phones = PhonesProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    clothes = ClothesProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    foods = FoodsProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    fav = FavProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    pharmacy = PharmacyProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    libraryproducts = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    spices = SpicesProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    supermarket = SupermarketProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    toys = ToysProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    accessories = AccessoriesProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    gifts = GiftsProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    birthday = BirthdayProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    socks = SocksProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    veils = VeilsProduct.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
-    # Combine all products
-    products = list(phones.values('name', 'price', 'primary_image')) + \
-               list(clothes.values('name', 'price', 'primary_image')) + \
-               list(fav.values('name', 'price', 'primary_image')) + \
-               list(pharmacy.values('name', 'price', 'primary_image')) + \
-               list(libraryproducts.values('name', 'price', 'primary_image')) + \
-               list(spices.values('name', 'price', 'primary_image')) + \
-               list(supermarket.values('name', 'price', 'primary_image')) + \
-               list(toys.values('name', 'price', 'primary_image')) + \
-               list(foods.values('name', 'price', 'primary_image'))
+    # Include 'description' field in the query
+    products = list(phones.values('name', 'price', 'primary_image', 'description')) + \
+               list(clothes.values('name', 'price', 'primary_image', 'description')) + \
+               list(fav.values('name', 'price', 'primary_image', 'description')) + \
+               list(pharmacy.values('name', 'price', 'primary_image', 'description')) + \
+               list(libraryproducts.values('name', 'price', 'primary_image', 'description')) + \
+               list(spices.values('name', 'price', 'primary_image', 'description')) + \
+               list(supermarket.values('name', 'price', 'primary_image', 'description')) + \
+               list(toys.values('name', 'price', 'primary_image', 'description')) + \
+               list(accessories.values('name', 'price', 'primary_image', 'description')) + \
+               list(gifts.values('name', 'price', 'primary_image', 'description')) + \
+               list(birthday.values('name', 'price', 'primary_image', 'description')) + \
+               list(socks.values('name', 'price', 'primary_image', 'description')) + \
+               list(veils.values('name', 'price', 'primary_image', 'description')) + \
+               list(foods.values('name', 'price', 'primary_image', 'description'))
 
-    # Convert the local file path to a full URL
+    # Convert image paths to absolute URLs
     for product in products:
         if product['primary_image']:
-            # Ensure the path is correct
             product['primary_image'] = request.build_absolute_uri(settings.MEDIA_URL + product['primary_image'])
         else:
-            # Provide a placeholder image if primary_image is missing
             product['primary_image'] = request.build_absolute_uri(static('products/placeholder.jpg'))
 
     return JsonResponse({"products": products})
