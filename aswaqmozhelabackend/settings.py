a1e4087913dd1b443a1763c50d968204b9ab09ec
Mostafa Mahran <mahranmostafa900@gmail.com>
2024-11-02 18:02:26 +0200

    Initial commit

--- aswaqmozhelabackend/settings.py
+++ aswaqmozhelabackend/settings.py
@@ -108,10 +108,21 @@ WSGI_APPLICATION = 'aswaqmozhelabackend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+#DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mozheladatabase',
+        'USER': 'mahrandatabase',
+        'PASSWORD': 'Sasamanar01009345616',
+        'HOST': 'mozheladatabase.ctuyy0ueu8yy.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
