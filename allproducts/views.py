ee09e3c94619658d28e777d36c593a17dfd57e92
Mostafa Mahran <mahranmostafa900@gmail.com>
2025-06-23 23:28:29 +0300

    latestproduct

--- allproducts/views.py
+++ allproducts/views.py
@@ -75,7 +75,7 @@ def get_latest_products(request):
                 'id': product.id,
                 'name': product.name,
                 'model_name': product.model_name,
-                'price': float(product.price),
+                'price': product.price,
                 'createAT': product.createAT,
                 'primary_image': request.build_absolute_uri(product.primary_image.url) if product.primary_image else request.build_absolute_uri(static('products/placeholder.jpg')),
                 'secondary_image1': request.build_absolute_uri(product.secondary_image1.url) if product.secondary_image1 else request.build_absolute_uri(static('products/placeholder.jpg')),
