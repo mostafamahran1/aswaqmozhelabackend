95aeca6ddbeaa8b7ff15da51c889c50e0fdfd384
Mostafa Mahran <mahranmostafa900@gmail.com>
2025-06-30 22:14:29 +0300

    sharebutton

--- allproducts/urls.py
+++ allproducts/urls.py
@@ -1,13 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from .views import search_all_products , GetModelNames ,  get_latest_products , get_product_by_model_and_id
+from .views import search_all_products , GetModelNames ,  get_latest_products
 
 urlpatterns = [
     path('search/', search_all_products, name='search_all_products'),
     path('model-names/', GetModelNames.as_view(), name='get_model_names'),
     path('latest-products/', get_latest_products, name='get_latest_products'),
-    path('product/', get_product_by_model_and_id, name='get_product_by_model_and_id'),
     # other URL patterns
 ]
 
