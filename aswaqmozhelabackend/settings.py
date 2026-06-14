0d56e60d848c48bf846a9d231d80e14ce7124d44
Mostafa Mahran <mahranmostafa900@gmail.com>
2026-06-14 18:54:20 +0300

    fix: updated settings page UI

--- aswaqmozhelabackend/settings.py
+++ aswaqmozhelabackend/settings.py
@@ -125,6 +125,7 @@ AWS_ACCESS_KEY_ID = 'AKIAYYWHOIXFR4AYYLPJ'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'aswaq-mozhela-media-storage'
 AWS_S3_REGION_NAME = 'us-east-1'
+AWS_QUERYSTRING_AUTH = False
 
 # تفعيل الـ S3 حصرياً لملفات الميديا (الصور المرفوعة)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
