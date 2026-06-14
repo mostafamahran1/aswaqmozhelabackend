da2c44245322266296f31c9751213d7304f9a476
Mostafa Mahran <mahranmostafa900@gmail.com>
2026-06-14 19:01:11 +0300

    fix: updated settings page UI

--- aswaqmozhelabackend/settings.py
+++ aswaqmozhelabackend/settings.py
@@ -129,6 +129,7 @@ AWS_QUERYSTRING_AUTH = False
 
 # تفعيل الـ S3 حصرياً لملفات الميديا (الصور المرفوعة)
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_DEFAULT_ACL = 'public-read'
 
 # الروابط التي سيقرأ منها أبلكيشن الفلوتر الصور مباشرة وبسرعة
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
